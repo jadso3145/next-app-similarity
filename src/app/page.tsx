1b88d2a2e387fab9b3647364c92d3e59d946e905
jadso3145 <2345jared@gmail.com>
2023-04-02 22:01:01 -0500

    creando un provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import { Inter } from "next/font/google";
-import Paragraph from "./components/ui/Paragraph";
+import Paragraph from "@/components/ui/Paragraph";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
    return (
       <main className="bg-red-500">
-         <Paragraph size={"sm"} className="jared" accessKey="test">text</Paragraph>
+         <Paragraph size={"sm"} className="jared" accessKey="test">
+            text
+         </Paragraph>
       </main>
    );
 }
